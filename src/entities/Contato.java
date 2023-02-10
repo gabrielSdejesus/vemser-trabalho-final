e43ea0b6667cf9803ec68275ede1935cdc76c9f1
gabrielSdejesus <gabrieldejesus9905@gmail.com>
2023-02-10 13:59:13 -0300

    feature-contato

---
+++ src/entities/Contato.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Contato {
+
+    private String telefone;
+    private String email;
+
+    public Contato(String telefone, String email) {
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
