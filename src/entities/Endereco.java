33be0c7d19e3bfd0398986537dad77557b05d6a1
Canovao <104451710+Canovao@users.noreply.github.com>
2023-02-10 12:45:42 -0300

    Innitial develop commit

---
+++ src/entities/Endereco.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Endereco {
+    private String logradouro, cidade, estado, pais, cep;
+
+    public Endereco(String logradouro, String cidade, String estado, String pais, String cep) {
+        this.logradouro = logradouro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+    }
+}
