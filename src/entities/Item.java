bef2b30e56874767f06029a21b2fd8de7c03e278
Canovao <104451710+Canovao@users.noreply.github.com>
2023-02-10 19:22:55 -0300

    Criação classe Item

---
+++ src/entities/Item.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Item {
+    private String nomeItem;
+    private double valor, quantidade;
+
+    public Item(String nomeItem, double valor, double quantidade) {
+        this.nomeItem = nomeItem;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+}
