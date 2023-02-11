eb0b6c77a5ee886f778d2c881160e93434253e8f
Canovao <104451710+Canovao@users.noreply.github.com>
2023-02-11 10:01:42 -0300

    Endereço e Contato prontos

--- src/entities/model/Contato.java
+++ src/entities/model/Contato.java
@@ -1,6 +1,8 @@
 package entities.model;
 
-public class Contato {
+import entities.interfaces.Alteracao;
+
+public class Contato implements Alteracao {
 
     private String telefone;
     private String email;
@@ -8,5 +10,23 @@ public class Contato {
     public Contato(String telefone, String email) {
         this.telefone = telefone;
         this.email = email;
+    }
+
+    @Override
+    public boolean alterarDado(String tipo, String novoDado) {
+        switch(tipo){
+            case "telefone" -> {
+                this.telefone = novoDado;
+                return true;
+            }
+            case "email" -> {
+                this.email = novoDado;
+                return true;
+            }
+            default -> {
+                System.err.println("Tipo inválido, erro na linha 25 do Contato");
+                return false;
+            }
+        }
     }
 }
--- src/entities/model/Endereco.java
+++ src/entities/model/Endereco.java
@@ -1,6 +1,8 @@
 package entities.model;
 
-public class Endereco {
+import entities.interfaces.Alteracao;
+
+public class Endereco implements Alteracao {
     private String logradouro, cidade, estado, pais, cep;
 
     public Endereco(String logradouro, String cidade, String estado, String pais, String cep) {
@@ -9,5 +11,35 @@ public class Endereco {
         this.estado = estado;
         this.pais = pais;
         this.cep = cep;
+    }
+
+    @Override
+    public boolean alterarDado(String tipo, String novoDado) {
+        switch(tipo){
+            case "logradouro" -> {
+                this.logradouro = novoDado;
+                return true;
+            }
+            case "cidade" -> {
+                this.cidade = novoDado;
+                return true;
+            }
+            case "estado" -> {
+                this.estado = novoDado;
+                return true;
+            }
+            case "pais" -> {
+                this.pais = novoDado;
+                return true;
+            }
+            case "cep" -> {
+                this.cep = novoDado;
+                return true;
+            }
+            default -> {
+                System.err.println("Tipo inválido, erro na linha 40 do Endereço");
+                return false;
+            }
+        }
     }
 }
