3e8160d79919e77b2226cda5bc4b5b3321a1822d
Joao Victor Lucio <joovitor.santos796@gmail.com>
2023-03-07 17:02:44 -0300

    implement Swagger

---
+++ src/main/java/br/com/dbc/vemser/financeiro/controller/OpenApiController.java
@@ -0,0 +1,21 @@
+package br.com.dbc.vemser.financeiro.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Hidden
+@RestController
+public class OpenApiController {
+
+    @Operation(hidden = true)
+    @GetMapping(value = "/")
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
+    }
+
+}
